b1ceb05f060a188fb3186a92a492bcd909b7a6fd
njokichristine <christinenjoki6900@gmail.com>
2023-03-25 03:22:29 +0300

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  def app_response(message: 'success', status: 200, data: nil)
+    render json: {
+        message: message,
+        data: data
+    }, status: status
+  end
+
 end
